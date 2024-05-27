64920515539656cfc13e3b81c7742fdb3fb59bd9
juyun <juyun7908@naver.com>
2024-05-27 19:48:56 +0900

    chapter 6 finish

---
+++ 06_02_clean_up/src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  const hiFn = () => {
+    console.log("created :)");
+    return byFn;
+  };
+  useEffect(hiFn, []);
+
+  // useEffect(() => {
+  //   console.log("created :)");
+  //   return () => console.log("destroyed :(");
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
